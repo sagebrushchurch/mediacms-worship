58df79bd15447ddcb86d4377612ca89b77afc0da
Jon Krake <1024jon@gmail.com>
2024-04-22 15:15:40 -0600

    update options

--- deploy/docker/local_settings.py
+++ deploy/docker/local_settings.py
@@ -1,6 +1,6 @@
-FRONTEND_HOST = 'http://localhost'
-PORTAL_NAME = 'MediaCMS'
-SECRET_KEY = 'ma!s3^b-cw!f#7s6s0m3*jx77a@riw(7701**(r=ww%w!2+yk2'
+FRONTEND_HOST = 'https://worec.sagebrush.work'
+PORTAL_NAME = 'Worship Recordings'
+SECRET_KEY = ''
 POSTGRES_HOST = 'db'
 REDIS_LOCATION = "redis://redis:6379/1"
 
@@ -11,7 +11,7 @@ DATABASES = {
         "HOST": POSTGRES_HOST,
         "PORT": "5432",
         "USER": "mediacms",
-        "PASSWORD": "mediacms",
+        "PASSWORD": "",
     }
 }
 
@@ -32,3 +32,23 @@ CELERY_RESULT_BACKEND = BROKER_URL
 MP4HLS_COMMAND = "/home/mediacms.io/bento4/bin/mp4hls"
 
 DEBUG = False
+LOGIN_ALLOWED = True
+REGISTER_ALLOWED = True
+UPLOAD_MEDIA_ALLOWED = True
+CAN_REPORT_MEDIA = False
+MAX_MEDIA_PER_PLAYLIST = 500
+UPLOAD_MAX_SIZE = 25000000000
+USERS_CAN_SELF_REGISTER = True
+USERS_NOTIFICATIONS = {
+    'MEDIA_ADDED': False,
+}
+CAN_ADD_MEDIA = "advancedUser"
+
+DEFAULT_FROM_EMAIL = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
+EMAIL_USE_TLS = True
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+ADMIN_EMAIL_LIST = ['']
